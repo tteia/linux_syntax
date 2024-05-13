329bca0fc894fa40b7da6bae3249b706a38e07fa
tteia <tteia.dl@gmail.com>
2024-05-13 12:50:51 +0900

    first

--- 1. basic_syntax.sh
+++ 1. basic_syntax.sh
@@ -1,2 +1,6 @@
 # 사용자가 현재 위치해 있는 폴더 경로 출력 명령어
 pwd
+
+# git add. >> 수정한 파일을 모두 load 하겠다! .이 모든 파일을 뜻함.
+# git commit -m "first file" >> commit > 확정하겠다 / m : message
+# git push origin main >> github에 올리겠다! (main에다 하겠다)
